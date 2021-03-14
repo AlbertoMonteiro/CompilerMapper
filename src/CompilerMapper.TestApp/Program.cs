28199c92f6e60c85d88fac921f0002d35ff57bd1
Alberto Monteiro <alberto.monteiro@live.com>
2021-03-14 17:00:33 -0300

    Initial commit

---
+++ src/CompilerMapper.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompilerMapper.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/CompilerMapper/CompilerMapperGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace CompilerMapper
+{
+    [Generator]
+    public class CompilerMapperGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+            => context.RegisterForSyntaxNotifications(() => new CompilerMapper());
+    }
+
+    public class CompilerMapper : ISyntaxReceiver
+    {
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
